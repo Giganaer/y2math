b9b5391421fc1873025add43247e78a1dcffe950
Giganaer <52987870+Giganaer@users.noreply.github.com>
2020-10-20 12:02:49 +0800

    Made t13_meansalaries.py more compact

--- t13_meansalaries.py
+++ t13_meansalaries.py
@@ -1,13 +1,11 @@
 # calculate mean salary
 
-weekly_salaries = [1000, 1100, 1200, 2100, 2500]
+salaries_to_employees = {1000: 20, 1100: 8, 1200: 10, 2100: 7, 2500: 5}
 
-num_employees = [20, 8, 10, 7, 5]
-print("Total number of employees:", sum(num_employees))
+total_employees = sum(salaries_to_employees.values())
+total_salaries = sum(salaries_to_employees)
 
-total_salaries = 0
-for i in range(len(weekly_salaries)):
-  total_salaries += weekly_salaries[i] * num_employees[i]
+print("Total number of employees:", total_employees)
 print("Total salary paid:", total_salaries)
 
-print("Mean weekly salary:", total_salaries / sum(num_employees))
+print("Mean weekly salary:", total_salaries / total_employees)
